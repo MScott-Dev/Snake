d0c89b2eabc44634c35c27e6e8a39c68c5429d0f
MScott-Dev <MScott0199@gmail.com>
2023-11-29 14:45:52 -0500

    Added game over for touching walls or the snake itself

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ let foodX, foodY;
 let snakeX = 5, snakeY = 10;
 let snakeBody = [];
 let velocityX = 0, velocityY = 0;
+let gameOver = false;
+let setIntervalId;
 
 // Randomly picks a grid area to put the food
 const changeFoodLocation = () => {
@@ -11,18 +13,25 @@ const changeFoodLocation = () => {
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+// Clears interval and reloads page to reset
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert('Game Over! Press OK to replay!');
+    location.reload();
+}
+
 // Changes directions based on key pressed 
 const changeDirection = (e) => {
-    if(e.key === 'ArrowUp') {
+    if(e.key === 'ArrowUp' && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if(e.key === 'ArrowDown') {
+    } else if(e.key === 'ArrowDown' && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if(e.key === 'ArrowLeft') {
+    } else if(e.key === 'ArrowLeft' && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if(e.key === 'ArrowRight') {
+    } else if(e.key === 'ArrowRight' && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
@@ -30,6 +39,9 @@ const changeDirection = (e) => {
 
 // Starts the game. Places the head and randomly picks food location
 const initGame = () => {
+
+    if(gameOver) return handleGameOver();
+
     let htmlMarkup = `<section class="food" style="grid-area: ${foodY} / ${foodX}"></section>`;
 
     // checks if head hits food and adds to length
@@ -53,10 +65,15 @@ const initGame = () => {
     for (let i = 0; i < snakeBody.length; i++) {
         // adds section for each body part
         htmlMarkup += `<section class="snakeHead" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></section>`;
+        // checks if snakebody touches itself if so game over
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
     }
 
+    // checks if snake touches wall if so then changes gameOver to true
     if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-        console.log("Game over!");
+        gameOver = true;
     }
 
    
@@ -68,6 +85,6 @@ const initGame = () => {
 
 changeFoodLocation();
 // Moves the head every 125ms 
-setInterval(initGame, 125);
+setIntervalId = setInterval(initGame, 125);
 
 document.addEventListener('keydown', changeDirection);
